309a2dfc0902d6d9fc3a955d7fc87a9427e1d4a0
iyacino <yacinetaamourt@gmail.com>
2023-02-04 00:22:59 +0100

    adding polling to our useQuery

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+import React, { useState } from "react"
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery, useMutation } from '@apollo/client';
 
  const client = new ApolloClient({
     uri: 'http://localhost:4000',
@@ -16,8 +16,14 @@ const GET_MESSAGES = gql`
   }
 `;
 
+const POST_MESSAGE = gql`
+  mutation ($user: String!, $content: String!){
+    postMessage(user: $user, content: $content)
+  }
+`;
+
 const Messages = ({ user }) =>{
-    const { data } = useQuery(GET_MESSAGES);
+    const { data } = useQuery(GET_MESSAGES, { pollInterval: 500});
     if(!data){
         return null;
     }
@@ -69,9 +75,78 @@ const Messages = ({ user }) =>{
 
 
 const Chat = () => {
+
+    const [message, setMessage] = useState({
+        user: 'yacine',
+        content: '',
+    })
+    const [postMessage] = useMutation(POST_MESSAGE);
+
+    const onSend = () =>{
+        if(message.content.length > 0){
+            postMessage({variables: message})
+        }
+        setMessage({...message, content: ''});
+    }
+
     return (
-        <div>
-            <Messages user="amine"/>
+        <div style={{
+            marginLeft: "400px",
+            marginRight: "400px"
+        }}>
+            <Messages user={message.user}/>
+            <div style={{
+                display: 'flex',
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <input style={{
+                    textAlign: "center",
+                    fontSize: 18,
+                    flex: 1,
+                    border: "1pt solid black",
+                    borderRadius: "8px",
+                    padding: 5,
+                    margin: 5,
+                    height: 30
+                }} 
+                label="user" 
+                value={message.user} 
+                onChange={(e) => setMessage({ ...message, user: e.target.value })}/>
+                <input style={{
+                    flex: 4,
+                    fontSize: "13px",
+                    border: "1pt solid black",
+                    borderRadius: "8px",
+                    padding: 5,
+                    margin: 5,
+                    height: 30
+                }}
+                label="content" 
+                value={message.content} 
+                onChange={(e)=> setMessage({ ...message, content: e.target.value })}
+                onKeyUp={(e) => {
+                    if(e.keyCode === 13){
+                        onSend();
+                    }
+                }}
+                />
+                <button style={{
+                    flex: 1,
+                    fontSize: "18px",
+                    border: "2pt solid black",
+                    borderRadius: "8px",
+                    padding: 5,
+                    background: "#242556",
+                    color: "white",
+                    width: "100px",
+                    height: "42px"
+                }} 
+                onClick={() => onSend()}>
+                    Send
+                </button>
+            </div>
+            
         </div>
     )
   }
